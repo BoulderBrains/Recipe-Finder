e196cd1c39fc85ffc7a64b188c530ec034b893ca
Restituo <42999733+Restituo@users.noreply.github.com>
2019-01-19 10:31:47 -0800

    Delete seeds.sql

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('R.C. Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheetos Burger', true);
