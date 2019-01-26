dde54d901b1200db2a147268b86c15c2839ab651
Thomas J Calhoun <thomascalhoun@Thomass-MacBook-Pro.local>
2019-01-26 09:45:35 -0800

    added title and signout button

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, passW) VALUES ('Thomas', 'password');
-INSERT INTO users (username, passW) VALUES ('Richard', 'password');
-INSERT INTO users (username, passW) VALUES ('Joshua', 'password', 'https://food52.com/recipes/11802-milk-peas');
-INSERT INTO users (username, passW) VALUES ('Seif', 'password', 'https://norecipes.com/chicken-paprikash/');
-INSERT INTO users (username, passW) VALUES ('Amy', 'password', 'https://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html'),;
+INSERT INTO users (username, passW, favorited) VALUES ('Thomas', 'password', 'https://www.bonappetit.com/recipe/cinnamon-sugar-apples');
+INSERT INTO users (username, passW, favorited) VALUES ('Richard', 'password', 'https://steamykitchen.com/18230-grilled-steak-and-tomato-salad-with-rum-vinaigrette-recipe.html');
+INSERT INTO users (username, passW, favorited) VALUES ('Joshua', 'password', 'https://food52.com/recipes/11802-milk-peas');
+INSERT INTO users (username, passW, favorited) VALUES ('Seif', 'password', 'https://norecipes.com/chicken-paprikash/');
+INSERT INTO users (username, passW, favorited) VALUES ('Amy', 'password', 'https://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html');
